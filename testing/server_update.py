3873ad2f330cabba8e69141ec5336c8271e74da5
Justin Abraham <jak13595@gmail.com>
2019-10-19 10:42:18 +0530

    Add files via upload

---
+++ testing/server_update.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('git config user.email "bas3dp@gmail.com"')
+os.system('git config user.name "BAS3DP"')
+os.system('git pull https://BAS3DP:{}{}{}plaabs3dp@github.com/prototype1995/ASRS-1.git master')
